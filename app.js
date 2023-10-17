fdc2d61ab4c936a7d06a3c4fb01ac8acd12cb024
EricMartinez <ericsantiago.m7@gmail.com>
2023-10-17 02:15:50 -0300

    subir cambios

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3030;
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 app.listen(PORT, () => console.log(`Server in running in http://localhost:${PORT}`))
