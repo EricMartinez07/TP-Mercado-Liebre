227610868c38df914e88d62a812e5bc5c791cfaf
EricMartinez <ericsantiago.m7@gmail.com>
2023-11-17 02:49:21 -0300

    subo formularios con todos los cambios

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 3030;
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/register', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'login.html')));
 app.listen(PORT, () => console.log(`Server in running in http://localhost:${PORT}`))
